237dfd2399b95796929b42e03be31d5098c95725
Lucas Rollemberg <lucas.rollemberg@gmail.com>
2025-12-02 21:45:04 -0300

    Creating fake preview for the challenge

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,30 @@
-function Home() {
-  return <h1>Welcome to TabNews Clone by kazluBR</h1>;
-}
+import Head from "next/head";
 
-export default Home;
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Amazon.com - Lava e Seca Samsung 11kg</title>
+        <meta
+          property="og:title"
+          content="🔥 Lava e Seca Samsung 11kg – 50% OFF na Amazon!"
+        />
+        <meta
+          property="og:description"
+          content="Oferta relâmpago! Lava e Seca Samsung 11kg com 50% de desconto apenas hoje. Aproveite antes que acabe!"
+        />
+        <meta
+          property="og:image"
+          content="https://m.media-amazon.com/images/I/51KPNZgowTL._AC_SL1000_.jpg"
+        />
+        <meta
+          property="og:url"
+          content="https://www.amazon.com.br/Samsung-Lavadora-Digital-Inverter-WW11T/dp/B0DBMMH3JY/"
+        />
+        <meta property="og:type" content="product" />
+      </Head>
+
+      <h1>Welcome to TabNews Clone by kazluBR</h1>
+    </>
+  );
+}
