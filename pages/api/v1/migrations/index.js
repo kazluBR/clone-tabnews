479122ea918e2a4105e9292b841d1210599b6166
Lucas Rollemberg <lucas.rollemberg@gmail.com>
2026-01-11 21:54:57 -0300

    Fixing /migrations endpoint connection bug

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,36 +3,46 @@ import { join } from "node:path"
 import database from "infra/database"
 
 export default async function migrations(request, response) {
-  const dbClient = await database.getNewClient()
-  const defaultMigrationOptions = {
-    dbClient: dbClient,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  }
-
-  if (request.method === "GET") {
-    const pendingMigrations = await migrationRunner(defaultMigrationOptions)
-    await dbClient.end()
-    return response.status(200).json(pendingMigrations)
-  }
-
-  if (request.method === "POST") {
-    const pendingMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dryRun: false,
+  const allowedMethods = ["GET", "POST"]
+  if (!allowedMethods.includes(request.method)) {
+    return response.status(405).json({
+      error: `Method "${request.method}" not allowed`,
     })
+  }
 
-    await dbClient.end()
-
-    if (pendingMigrations.length > 0) {
-      return response.status(201).json(pendingMigrations)
+  let dbClient
+  try {
+    dbClient = await database.getNewClient()
+    const defaultMigrationOptions = {
+      dbClient: dbClient,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
     }
 
-    return response.status(200).json(pendingMigrations)
-  }
+    if (request.method === "GET") {
+      const pendingMigrations = await migrationRunner(defaultMigrationOptions)
+      return response.status(200).json(pendingMigrations)
+    }
 
-  response.status(405).end()
+    if (request.method === "POST") {
+      const pendingMigrations = await migrationRunner({
+        ...defaultMigrationOptions,
+        dryRun: false,
+      })
+
+      if (pendingMigrations.length > 0) {
+        return response.status(201).json(pendingMigrations)
+      }
+
+      return response.status(200).json(pendingMigrations)
+    }
+  } catch (error) {
+    console.error(error)
+    throw error
+  } finally {
+    await dbClient.end()
+  }
 }
