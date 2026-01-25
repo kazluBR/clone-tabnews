c4c0e6fe5620e21ca1b8ddafc42e9e569a5406fe
Lucas Rollemberg <lucas.rollemberg@gmail.com>
2026-01-25 08:41:22 -0300

    style: add missing comma to /api/v1/status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -7,14 +7,14 @@ async function status(request, response) {
   const databaseVersionValue = databaseVersionResult.rows[0].server_version
 
   const databaseMaxConnectionsResult = await database.query(
-    "SHOW max_connections;"
+    "SHOW max_connections;",
   )
   const databaseMaxConnectionsValue =
     databaseMaxConnectionsResult.rows[0].max_connections
 
   const databaseName = process.env.POSTGRES_DB
   const databaseOpenedConnectionsResult = await database.query(
-    `SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = '${databaseName}';`
+    `SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = '${databaseName}';`,
   )
   const databaseOpenedConnectionsValue =
     databaseOpenedConnectionsResult.rows[0].count
