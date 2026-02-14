e515dfdd73187b9914098a0785feddcc62dcc13f
Lucas Rollemberg <lucas.rollemberg@gmail.com>
2026-02-13 22:02:38 -0300

    feat: create user.findOneByUsername method and /api/v1/users/[username] endpoint

--- models/user.js
+++ models/user.js
@@ -1,5 +1,36 @@
 import database from "infra/database"
-import { ValidationError } from "infra/errors"
+import { ValidationError, NotFoundError } from "infra/errors"
+
+async function findByUsername(username) {
+  const userFound = await runSelectQuery(username)
+
+  return userFound
+
+  async function runSelectQuery(username) {
+    const results = await database.query({
+      text: `
+        SELECT  
+          *
+        FROM
+          users
+        WHERE
+          LOWER(username) = LOWER($1)
+        LIMIT 
+          1
+        ;`,
+      values: [username],
+    })
+
+    if (results.rowCount === 0) {
+      throw new NotFoundError({
+        message: "O username informado não foi encontrado no sistema.",
+        action: "Verifique se o username está digitado corretamente.",
+      })
+    }
+
+    return results.rows[0]
+  }
+}
 
 async function create(userInputValues) {
   await validateUniqueEmail(userInputValues.email)
@@ -82,6 +113,7 @@ async function create(userInputValues) {
 
 const user = {
   create,
+  findByUsername,
 }
 
 export default user
