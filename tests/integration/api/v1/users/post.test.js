7048ef341df336a6b5a271a96d953033abca7e9e
Lucas Rollemberg <lucas.rollemberg@gmail.com>
2026-02-16 09:38:12 -0300

    feat: add support for PATCH on /api/v1/users/[username]

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -41,7 +41,7 @@ describe("POST /api/v1/users", () => {
       expect(Date.parse(responseBody.created_at)).not.toBeNaN()
       expect(Date.parse(responseBody.updated_at)).not.toBeNaN()
 
-      const userInDatabase = await user.findByUsername("kazluBR")
+      const userInDatabase = await user.findOneByUsername("kazluBR")
       const correctPasswordMatch = await password.compare(
         "senha123",
         userInDatabase.password,
@@ -90,7 +90,7 @@ describe("POST /api/v1/users", () => {
       expect(response2Body).toEqual({
         name: "ValidationError",
         message: "O email informado já está sendo utilizado.",
-        action: "Utilize outro email para realizar o cadastro.",
+        action: "Utilize outro email para realizar esta operação.",
         status_code: 400,
       })
     })
@@ -129,7 +129,7 @@ describe("POST /api/v1/users", () => {
       expect(response2Body).toEqual({
         name: "ValidationError",
         message: "O email informado já está sendo utilizado.",
-        action: "Utilize outro email para realizar o cadastro.",
+        action: "Utilize outro email para realizar esta operação.",
         status_code: 400,
       })
     })
@@ -168,7 +168,7 @@ describe("POST /api/v1/users", () => {
       expect(response2Body).toEqual({
         name: "ValidationError",
         message: "O username informado já está sendo utilizado.",
-        action: "Utilize outro username para realizar o cadastro.",
+        action: "Utilize outro username para realizar esta operação.",
         status_code: 400,
       })
     })
