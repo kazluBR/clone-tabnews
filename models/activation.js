55f543ed2795c57a7b34005804f56b42d54f6d0b
Lucas Rollemberg <lucas.rollemberg@gmail.com>
2026-03-06 11:29:58 -0300

    feat: create user activation token

--- models/activation.js
+++ models/activation.js
@@ -1,13 +1,64 @@
 import email from "infra/email"
+import database from "infra/database"
+import webserver from "infra/webserver"
 
-async function sendEmailToUser(user) {
+const EXPIRATION_IN_MILLISECONDS = 60 * 15 * 1000 // 15 minutes
+
+async function findOneByUserId(userId) {
+  const newToken = await runSelectQuery(userId)
+  return newToken
+
+  async function runSelectQuery(userId) {
+    const results = await database.query({
+      text: `
+        SELECT
+          *
+        FROM
+          user_activation_tokens
+        WHERE
+          user_id = $1
+        LIMIT 
+          1
+        ;`,
+      values: [userId],
+    })
+    return results.rows[0]
+  }
+}
+
+async function create(userId) {
+  const expiresAt = new Date(Date.now() + EXPIRATION_IN_MILLISECONDS)
+
+  const newToken = await runInsertQuery(userId, expiresAt)
+  return newToken
+
+  async function runInsertQuery(userId, expiresAt) {
+    const results = await database.query({
+      text: `
+        INSERT INTO
+          user_activation_tokens (user_id, expires_at)
+        VALUES
+          ($1, $2)  
+        RETURNING
+          *
+        ;`,
+      values: [userId, expiresAt],
+    })
+
+    return results.rows[0]
+  }
+}
+
+async function sendEmailToUser(user, activationToken) {
+  console.log("Enviando email de ativação para o usuário:", user.email)
+  console.log("Token de ativação:", activationToken)
   await email.send({
     from: "KazluNews <contato@kazlunews.com.br>",
     to: user.email,
     subject: "Ative seu cadastro no KazluNews",
     text: [
       `${user.username}, clique no link abaixo para ativar seu cadastro no KazluNews:\n`,
-      "https://...\n",
+      `${webserver.origin}/cadastro/ativar/${activationToken.id}\n`,
       "Atenciosamente",
       "Equipe KazluNews",
     ].join("\n"),
@@ -15,6 +66,8 @@ async function sendEmailToUser(user) {
 }
 
 const activation = {
+  findOneByUserId,
+  create,
   sendEmailToUser,
 }
 
--- tests/integration/_use-cases/registration-flow.test.js
+++ tests/integration/_use-cases/registration-flow.test.js
@@ -1,4 +1,5 @@
 import orchestrator from "tests/orchestrator"
+import activation from "models/activation"
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices()
@@ -8,6 +9,8 @@ beforeAll(async () => {
 })
 
 describe("User case: Registration flow (all successful)", () => {
+  let createUserResponseBody
+
   test("Create user account", async () => {
     const createUserResponse = await fetch(
       "http://localhost:3000/api/v1/users",
@@ -26,7 +29,7 @@ describe("User case: Registration flow (all successful)", () => {
 
     expect(createUserResponse.status).toBe(201)
 
-    const createUserResponseBody = await createUserResponse.json()
+    createUserResponseBody = await createUserResponse.json()
 
     expect(createUserResponseBody).toEqual({
       id: createUserResponseBody.id,
@@ -42,10 +45,15 @@ describe("User case: Registration flow (all successful)", () => {
   test("Receive activation email", async () => {
     const lastEmail = await orchestrator.getLastEmail()
 
+    const activationToken = await activation.findOneByUserId(
+      createUserResponseBody.id,
+    )
+
     expect(lastEmail.sender).toBe("<contato@kazlunews.com.br>")
     expect(lastEmail.recipients[0]).toBe("<registration-flow@curso.dev>")
     expect(lastEmail.subject).toBe("Ative seu cadastro no KazluNews")
     expect(lastEmail.text).toContain("registrationFlow")
+    expect(lastEmail.text).toContain(activationToken.id)
   })
 
   test("Activate account", async () => {})
